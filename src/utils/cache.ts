56ad46ef0f2ebbb4c64e9f02fcd1cdd1a2463504
qwertyyb <qwertyyb@foxmail.com>
2025-06-04 12:00:03 +0800

    fix: 修复 http 下无法正常加载书架的问题

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,10 +2,10 @@ import Logger from "js-logger"
 
 const logger = Logger.get('cache')
 
-let cachePromise = window.caches?.open('e-reader')
+let cachePromise: Promise<Cache> | undefined = window.caches?.open('e-reader')
 
-export const getCache = <T>(key: string): Promise<T | null | undefined> => {
-  return cachePromise.then(cache => cache.match(key))
+export const getCache = <T>(key: string): Promise<T | null | undefined> | undefined => {
+  return cachePromise?.then(cache => cache.match(key))
     .then(r => r?.json())
     .catch(err => {
       logger.error(`cache.get ${key} error`, err)
@@ -13,8 +13,8 @@ export const getCache = <T>(key: string): Promise<T | null | undefined> => {
     })
 }
 
-export const setCache = <T>(key: string, data: T): Promise<void | false> => {
-  return cachePromise.then(cache => cache.put(key, new Response(JSON.stringify(data))))
+export const setCache = <T>(key: string, data: T): Promise<void | false> | undefined => {
+  return cachePromise?.then(cache => cache.put(key, new Response(JSON.stringify(data))))
     .catch(err => {
       logger.error(`cache.set ${key} error`, data, err)
       return false
